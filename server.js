4ba0b79d899a18b4d46b9e59bef906e7be11e06a
maksimLobachevskiy <lobachevskiy.maksim@gmail.com>
2021-08-15 23:52:16 +0300

    routes

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const posts = require("./routes/api/post");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, './build')));
 
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
 app.use(
